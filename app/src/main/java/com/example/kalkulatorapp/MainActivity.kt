19a0c3e61b68bb1beadddff597168a2b6bf9db8b
falihnaufal17 <falihnaufal1700@gmail.com>
2022-02-05 22:25:13 +0700

    feat: ui and function

--- app/src/main/java/com/example/kalkulatorapp/MainActivity.kt
+++ app/src/main/java/com/example/kalkulatorapp/MainActivity.kt
@@ -2,10 +2,71 @@ package com.example.kalkulatorapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
+import net.objecthunter.exp4j.ExpressionBuilder
 
 class MainActivity : AppCompatActivity() {
+    lateinit var txtInput: TextView
+    var lastNumeric: Boolean = false
+    var stateError: Boolean = false
+    var lastDot: Boolean = false
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        txtInput = findViewById(R.id.txtInput)
+    }
+
+    fun onDigit(view: View) {
+        if (stateError) {
+            txtInput.text = (view as Button).text
+            stateError = false
+        } else {
+            txtInput.append((view as Button).text)
+        }
+
+        lastNumeric = true
+    }
+
+    fun onDecimalPoint() {
+        if (lastNumeric && !stateError && !lastDot) {
+            txtInput.append(".")
+            lastNumeric = false
+            lastDot = true
+        }
+    }
+
+    fun onOperator(view: View) {
+        if (lastNumeric && !stateError) {
+            txtInput.append((view as Button).text)
+            lastNumeric = false
+            lastDot = false
+        }
+    }
+
+    fun onClear() {
+        this.txtInput.text = ""
+        lastNumeric = false
+        lastDot = false
+        stateError = false
+    }
+
+    fun onEqual() {
+        if (lastNumeric && !stateError) {
+            val txt = txtInput.text.toString()
+            val expression = ExpressionBuilder(txt).build()
+            try {
+                val result = expression.evaluate()
+                txtInput.text = result.toString()
+                lastDot = true
+            } catch (ex: ArithmeticException) {
+                txtInput.text = "Error"
+                stateError = true
+                lastNumeric = false
+            }
+        }
     }
 }
